ad790bef1ac5ce356ead2f456ac1c06a4e9855de
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-03-10 09:35:40 +0100

    feat: ad function to process articles

---
+++ code/processArticles.R
@@ -0,0 +1,138 @@
+# Project:   imputationSociology
+# Objective: Function to precess articles
+# Author:    Edoardo Costantini
+# Created:   2022-03-09
+# Modified:  2022-03-09
+
+processArticles <- function(file_names, file_paths,
+                            first_stop, last_stop,
+                            encoding = "UTF-8",
+                            par_length = 5) {
+  # Internals -------------------------------------------------------------
+
+  # file_names = files_AJS
+  # file_paths = path_AJS
+  # first_stop = "More"
+  # last_stop  = "References"
+  # encoding   = "UTF-8"
+  # par_length = 5 # number of words to extract for context
+
+  # Body ------------------------------------------------------------------
+
+  # Check internal directories
+  path_internal <- list.dirs(file_paths, recursive = FALSE)
+  if(length(path_internal) != 0){
+    files_ASR <- sapply(path_internal, function (x){
+      paste0(x, "/", list.files(x))
+    })
+
+    # List of files for the ASR journal
+    files_ASR <- unlist(files_ASR)
+  }
+
+  # Load text of files
+  articles <- lapply(paste0(file_paths, file_names),
+                     extractHTMLtext,
+                     first_stop = first_stop,
+                     last_stop = last_stop,
+                     encoding = encoding
+  )
+
+  # Give names
+  names(articles) <- file_names
+
+  # Create a corpus
+  corp <- tm::Corpus(VectorSource(articles))
+
+  # Create quanteda tokens
+  q_corp <- quanteda::corpus(corp)
+  toks <- quanteda::tokens(q_corp)
+
+  # Mentions of survey / sample ------------------------------------------------
+
+  suv_phrases <- c("sample*",
+                   "survey*")
+  sta_phrases <- c("significan*",
+                   "standard error",
+                   "confidence interval*",
+                   "pvalue",
+                   "p-value"
+  )
+  suv_df <- extracPhrases(toks, suv_phrases)
+  sta_df <- extracPhrases(toks, sta_phrases)
+
+  # Papers that mention surveys and do inference
+  pp_index <- unique(suv_df$docname) %in% unique(sta_df$docname)
+  pp_selected <- unique(suv_df$docname)[pp_index]
+
+  # Mentions of missing values -------------------------------------------------
+
+  # Define possible ways of referring to missing values
+  NA_phrases <- c("missing value*",
+                  "missing case*",
+                  "missing observation*",
+                  "missing row*",
+                  "nonresponse",
+                  "non-response"
+  )
+
+  # Run extraction
+  NA_df <- extracPhrases(toks[pp_selected], NA_phrases, window = par_length)
+
+  # Mentions of imputation -----------------------------------------------------
+
+  imp_phrases <- c("imputation*",
+                   "single imputation*",
+                   "multiple imputation*")
+
+  imp_df <- extracPhrases(toks[pp_selected], imp_phrases, window = par_length)
+
+  # Identify original papers filenames
+  mi_papers <- file_names[as.numeric(gsub("text",
+                                          "",
+                                          unique(imp_df$docname)))]
+
+  # Make the object a data.frame
+  imp_code <- as.data.frame(imp_df)
+
+  # Keep the text id, pre, keyword, and post columns
+  important_cols <- colnames(imp_code)[c(1, 4, 5, 6)]
+  imp_code <- imp_code[, important_cols]
+
+  # Collapse the pre, keyword, and post columns into paragraphs
+  imp_code$paragraph <- apply(imp_code, 1, function (x){
+    paste0(x[important_cols[-1]], collapse = " ")
+  })
+
+  # Keep only id and paragraph
+  imp_code <- imp_code[, c("docname", "paragraph")]
+
+  # Collapse all paragraphs into one
+  fullParagraphs <- sapply(unique(imp_code$docname), function(x){
+    paste0(imp_code[imp_code$docname == x, "paragraph"],
+           collapse = " \n \n [...] ")
+  })
+
+  # Create data.frame for annotation
+  imp_xls <- data.frame(textName = unique(imp_code$docname),
+                        tag_im = "", # actually does some form of imputation
+                        tag_mi = "", # performs mi
+                        tag_nm = "", # mentions number of imputation
+                        tag_pr = "", # describes which predictors are used in the imputation models
+                        notes = "")
+
+  # Give meaningful rownames to make it easier for coding
+  rownames(imp_xls) <- mi_papers
+
+  # Order by file name
+  imp_xls <- imp_xls[order(mi_papers), ]
+
+  # Output
+  return(
+    list(coding = imp_xls,
+         n_papers = length(pp_selected),
+         paragraphs = fullParagraphs,
+         topic_NA = NA_df,
+         topic_MI = imp_df)
+  )
+}
--- code/readingArticles.R
+++ code/readingArticles.R
@@ -1,5 +1,5 @@
 # Project:   imputationSociology
-# Objective: TODO
+# Objective: Coding artciels from different journals
 # Author:    Edoardo Costantini
 # Created:   2022-03-04
 # Modified:  2022-03-09
@@ -7,6 +7,7 @@
   rm(list = ls())
   source("extractHTMLtext.R")
   source("extractPhrases.R")
+  source("processArticles.R")
 
  # Load packages
   library(XML)
@@ -17,175 +18,149 @@
 
   # Define the location of the pdfs
   path_AJS <- "../input/ALS-2017-2021/"
-  path_ASR <- "../input/ASR"
-    path_ASR_internal <- list.dirs(path_ASR, recursive = FALSE)
-  files_ASR <- sapply(path_ASR_internal, function (x){
-    paste0(x, "/", list.files(x))
-  })
-
-  # List of files for the ASR journal
-  files_ASR <- unlist(files_ASR)
+  path_ASR <- "../input/ASR-2017-2021/"
 
   # Find what htmls you have in the input folder
   files_AJS <- list.files(path = path_AJS,
                       pattern = "html$")
+  files_ASR <- list.files(path = path_ASR,
+                          pattern = "html$")
 
-  # Load the texts in your sessions by using extractHTMLtext
-  articles <- lapply(paste0(path_AJS, files_AJS), extractHTMLtext,
-                     first_stop = "More",
-                     last_stop = "References",
-                     encoding = "UTF-8"
-  )
 
-  # Load ASR htmls
-  articles <- lapply(files_ASR,
-                     extractHTMLtext,
-                     first_stop = "Article Information",
-                     last_stop = "References",
-                     encoding = "UTF-8"
-  )
+  # Process AJS
+  AJS <- processArticles(file_names = files_AJS,
+                         file_paths = path_AJS,
+                         first_stop = "More",
+                         last_stop = "References")
+  ASR <- processArticles(file_names = files_ASR,
+                         file_paths = path_ASR,
+                         first_stop = "Article Information",
+                         last_stop = "References")
 
-  # Give names
-  names(articles) <- files_AJS
+# Decriptives ------------------------------------------------------------------
 
-  # Create a corpus
-  corp <- tm::Corpus(VectorSource(articles))
+  # Mentions of NAs
+  length(unique(AJS$topic_NA$docname))
+  length(unique(AJS$topic_NA$docname))/length(AJS$n_papers) * 100
 
-  # Create quanteda tokens
-  q_corp <- quanteda::corpus(corp)
-  toks <- quanteda::tokens(q_corp)
+  # Mentions of imputation
+  length(unique(AJS$topic_MI$docname))
+  length(unique(AJS$topic_MI$docname))/length(unique(AJS$topic_NA$docname)) * 100
 
-# Mentions of survey / sample --------------------------------------------------
+# Coding of papers -------------------------------------------------------------
 
-  suv_phrases <- c("sample*",
-                   "survey*")
-  sta_phrases <- c("significan*",
-                   "standard error",
-                   "confidence interval*",
-                   "pvalue",
-                   "p-value"
-  )
-  suv_df <- extracPhrases(toks, suv_phrases, window = 3)
-  sta_df <- extracPhrases(toks, sta_phrases, window = 3)
-
-  # Papers that mention surveys and do inference
-  pp_index <- unique(suv_df$docname) %in% unique(sta_df$docname)
-  pp_selected <- unique(suv_df$docname)[pp_index]
-
-# Mentions of missing values ---------------------------------------------------
-
-  # Define possible ways of referring to missing values
-  NA_phrases <- c("missing value*",
-                  "missing case*",
-                  "missing observation*",
-                  "missing row*",
-                  "nonresponse",
-                  "non-response"
-  )
-
-  # Run extraction
-  NA_df <- extracPhrases(toks[pp_selected], NA_phrases, window = 20)
-  length(unique(NA_df$docname))
-  length(unique(NA_df$docname))/length(pp_selected) * 100
-
-# Mentions of imputation -------------------------------------------------------
-
-  imp_phrases <- c("imputation*",
-                   "single imputation*",
-                   "multiple imputation*")
-
-  imp_df <- extracPhrases(toks[pp_selected], imp_phrases, window = 50)
-  length(unique(imp_df$docname))
-  length(unique(imp_df$docname))/length(unique(NA_df$docname)) * 100
-
-  # Identify original papers filenames
-  mi_papers <- files_AJS[as.numeric(gsub("text",
-                                         "",
-                                         unique(imp_df$docname)))]
-  mi_papers <- files_ASR[as.numeric(gsub("text",
-                                         "",
-                                         unique(imp_df$docname)))]
-
-  # Make the object a data.frame
-  imp_xls <- as.data.frame(imp_df)
-
-  # Keep the text id, pre, keyword, and post columns
-  important_cols <- colnames(imp_xls)[c(1, 4, 5, 6)]
-  imp_xls <- imp_xls[, important_cols]
-
-  # Collapse the pre, keyword, and post columns into paragraphs
-  imp_xls$paragraph <- apply(imp_xls, 1, function (x){
-    paste0(x[important_cols[-1]], collapse = " ")
+  # Copy all of the html files to manually scan to named output directories
+  lapply(rownames(AJS$coding), function (x){
+    system(command = paste0("cp ", path_AJS, x, " ../output/AJS/"))
+  })
+  lapply(rownames(ASR$coding), function (x){
+    system(command = paste0("cp ", path_ASR, x, " ../output/ASR/"))
   })
 
-  # Keep only id and paragraph
-  imp_xls <- imp_xls[, c("docname", "paragraph")]
+  # Columns to fill with manual coding
+  code_cols <- c("tag_im", # Was imputation actually performed? 0 = no, 1 = yes
+                 "tag_mi", # Was multiple imputation actually performed? 0 = no, 1 = yes
+                 "tag_nm", # Was the number of imputation reported? 0 = no, 1 = yes
+                 "tag_pr") # Were the predictors for the imputation models described? 0 = no, 1 = yes
 
-  # Collapse all paragraphs into one
-  fullParagraphs <- sapply(unique(imp_xls$docname), function(x){
-    paste0(imp_xls[imp_xls$docname == x, "paragraph"],
-           collapse = " \n \n [...] ")
-  })
-
-  # Create data.frame for annotation
-  imp_xls <- data.frame(fileName = mi_papers,
-                        textName = unique(imp_xls$docname),
-                        tag_im = "", # actually does some form of imputation
-                        tag_mi = "", # performs mi
-                        tag_nm = "", # mentions number of imputation
-                        tag_pr = "", # describes which predictors are used in the imputation models
-                        notes = "")
-
-  # Give meaningful rownames to make it easier for coding
-  rownames(imp_xls) <- mi_papers
-
-  # Order the rows by fileName
-  imp_xls <- imp_xls[order(imp_xls$fileName), ]
-
-  # Manually code the text data
-  imp_xls["690053.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(0, 0, 0, 0)
-  imp_xls["691261.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["691327.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 0, 0, 0)
-  imp_xls["692350.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(0, 0, 0, 0)
-  imp_xls["693703.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["696209.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(0, 0, 0, 0)
-  imp_xls["697111.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["697498.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 0, 0, 0)
-  imp_xls["697528.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 0, 0, 0)
-  imp_xls["698481.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 1)
-  imp_xls["699652.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["702008.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["702278.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["703044.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 1)
-  imp_xls["707985.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 1)
-  imp_xls["709778.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(0, 0, 0, 0)
-  imp_xls["711231.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["712406.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(0, 0, 0, 0)
-  imp_xls["712972.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["714062.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 1, 0)
-  imp_xls["717448.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 1, 0, 0)
-  imp_xls["718451.html", c("tag_im", "tag_mi", "tag_nm", "tag_pr")] <- c(1, 0, 0, 0)
+  # > Manually code AJS papers -------------------------------------------------
+  AJS$coding["690053.html", code_cols] <- c(0, 0, 0, 0)
+  AJS$coding["691261.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["691327.html", code_cols] <- c(1, 0, 0, 0)
+  AJS$coding["692350.html", code_cols] <- c(0, 0, 0, 0)
+  AJS$coding["693703.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["696209.html", code_cols] <- c(0, 0, 0, 0)
+  AJS$coding["697111.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["697498.html", code_cols] <- c(1, 0, 0, 0)
+  AJS$coding["697528.html", code_cols] <- c(1, 0, 0, 0)
+  AJS$coding["698481.html", code_cols] <- c(1, 1, 1, 1) # all analysis models variables
+  AJS$coding["699652.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["702008.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["702278.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["703044.html", code_cols] <- c(1, 1, 1, 1)
+  AJS$coding["707985.html", code_cols] <- c(1, 1, 1, 1) # all independent variables in our analyses
+  AJS$coding["709778.html", code_cols] <- c(0, 0, 0, 0)
+  AJS$coding["711231.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["712406.html", code_cols] <- c(0, 0, 0, 0)
+  AJS$coding["712972.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["714062.html", code_cols] <- c(1, 1, 1, 0)
+  AJS$coding["717448.html", code_cols] <- c(1, 1, 0, 0)
+  AJS$coding["718451.html", code_cols] <- c(1, 0, 0, 0)
 
   # Draw conclusions
+  # Total survey/sample + inference articles
+  AJS$n_papers
+
   # Papers doing actual imputation
-  sum(imp_xls$tag_im == 1)
+  sum(AJS$coding$tag_im == 1)
 
   # Papers doing multiple imputation
-  sum(imp_xls$tag_mi == 1)
+  sum(AJS$coding$tag_mi == 1)
 
   # Papers descirbing how many imputations
-  sum(imp_xls$tag_nm == 1)
+  sum(AJS$coding$tag_nm == 1)
 
   # Papers descirbing which predictors are included in the imputation model
-  sum(imp_xls$tag_pr == 1)
+  rownames(AJS$coding[which(AJS$coding$tag_pr == 1), ])
+  sum(AJS$coding$tag_pr == 1)
 
   # Add paragraphs for excel sheet
-  imp_xls$paragraphs <- fullParagraphs
+  AJS$coding$paragraphs <- AJS$paragraphs
 
   # Write in an excel sheet
-  write_xlsx(imp_xls,"../output/papersImputing.xlsx")
+  write_xlsx(AJS$coding,"../output/papersImputing.xlsx")
 
-  # Copy all of the html files imputing to the output directory
-  lapply(mi_papers, function (x){
-    system(command = paste0("cp ", path_AJS, x, " ../output/papersImputing/"))
-  })
+  # > Manually code ASR papers -------------------------------------------------
+  ASR$coding["0003122416684777.html" , code_cols] <- c(1, 1, 1, 0)
+  ASR$coding["0003122416686521.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122417691952.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122417701115.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122417706463.html" , code_cols] <- c(1, 0, 0, 0)
+  ASR$coding["0003122417709294.html" , code_cols] <- c(1, 1, 1, 0)
+  ASR$coding["0003122417712729.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122417714422.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122417716611.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122417724001.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122417734353.html" , code_cols] <- c(1, 1, 1, 1) # as a function of the other variables in the analysis
+  ASR$coding["0003122417751442.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122418759651.html" , code_cols] <- c(1, 1, 0, 1) # uses two precise variables
+  ASR$coding["0003122418773353.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122418785368.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122418792836.html" , code_cols] <- c(1, 1, 1, 1) # all variables as predictors
+  ASR$coding["0003122418795856.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122418808005.html" , code_cols] <- c(1, 1, 1, 1)
+  ASR$coding["0003122418820702.html" , code_cols] <- c(1, 1, 1, 0)
+  ASR$coding["0003122418821339.html" , code_cols] <- c(1, 1, 1, 0)
+  ASR$coding["0003122418823184.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122419832497.html" , code_cols] <- c(1, 1, 0, 1)
+  ASR$coding["0003122419847165.html" , code_cols] <- c(1, 1, 1, 0)
+  ASR$coding["0003122419848723.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122419885094.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122419886550.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122420930018.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["0003122420967647.html" , code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["0003122421996686.html" , code_cols] <- c(1, 1, 0, 0)
+  ASR$coding["00031224211004187.html", code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["00031224211027800.html", code_cols] <- c(0, 0, 0, 0)
+  ASR$coding["00031224211038507.html", code_cols] <- c(1, 1, 1, 1) # all variables!
+  ASR$coding["00031224211056267.html", code_cols] <- c(0, 0, 0, 0)
+
+  # Draw conclusions
+  # Total survey/sample + inference articles
+  ASR$n_papers
+
+  # Papers doing actual imputation
+  sum(ASR$coding$tag_im == 1)
+
+  # Papers doing multiple imputation
+  sum(ASR$coding$tag_mi == 1)
+
+  # Papers descirbing how many imputations
+  sum(ASR$coding$tag_nm == 1)
+
+  # Papers descirbing which predictors are included in the imputation model
+  rownames(ASR$coding[which(ASR$coding$tag_pr == 1), ])
+  sum(ASR$coding$tag_pr == 1)
+
+  # Add paragraphs for excel sheet
+  ASR$coding$paragraphs <- ASR$paragraphs
